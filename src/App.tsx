f21187235413c73a0b04d4f43dfdac0a54c6f789
Razzula <jack02gillespie@gmail.com>
2024-09-12 18:22:56 +0200

    Implement lump sum transfers into calculations

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,9 @@ export interface Account {
   compoundRate: number;
   compoundOffset: number;
   state?: string;
+
+  minInflow?: number;
+  maxInflow?: number;
 }
 
 function App() {
@@ -23,8 +26,8 @@ function App() {
   // Define the accounts array with typed objects
   const accounts: Account[] = [
     { name: 'Natwest First Saver', type: 'savings', initialDeposit: 1000, annualInterestRate: 2.67, compoundRate: 3, compoundOffset: -1, state: 'owned' },
-    { name: 'Natwest Digital Regular Saver', type: 'regular saver', initialDeposit: 0, annualInterestRate: 6, compoundRate: 1, compoundOffset: 0, state: 'owned' },
-    { name: 'Test', type: 'regular saver', initialDeposit: 0, annualInterestRate: 6, compoundRate: 1, compoundOffset: 0 },
+    { name: 'Natwest Digital Regular Saver', type: 'regular saver', initialDeposit: 0, annualInterestRate: 6, compoundRate: 1, compoundOffset: 0, state: 'owned', maxInflow: 150 },
+    { name: 'Test', type: 'regular saver', initialDeposit: 0, annualInterestRate: 5, compoundRate: 1, compoundOffset: 0, maxInflow: 150 },
   ];
 
   // Refs for SegmentedControl segments
--- src/BankAccountTable.tsx
+++ src/BankAccountTable.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Account} from './App';
+import React, { useEffect } from 'react';
+import { Account } from './App';
 
 import './App.css';
 
@@ -9,41 +9,124 @@ interface BankAccountTableProps {
     setTotalDelta: (totalDelta: number) => void;
 }
 
-const months: string[] = [ "SEP", "OCT", "NOV", "DEC", "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG"] ;
+const months: string[] = ["SEP", "OCT", "NOV", "DEC", "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG"];
 
-const calculateMonthlyBalance = (initialDeposit: number, annualInterestRate: number, compoundRate: number, compoundOffset: number) => {
-    const monthlyInterestRate = annualInterestRate / 100 / 12;
-    const balanceArray: number[] = [];
+// const calculateMonthlyBalance = (initialDeposit: number, annualInterestRate: number, compoundRate: number, compoundOffset: number) => {
+//     const monthlyInterestRate = annualInterestRate / 100 / 12;
+//     const balanceArray: number[] = [];
 
-    let currentBalance = initialDeposit;
+//     let currentBalance = initialDeposit;
 
-    let store = 0;
+//     let store = 0;
 
+//     for (let month = 1; month <= 12; month++) {
+
+//         const interest = currentBalance * monthlyInterestRate;
+//         store += interest;
+
+//         if ((month - compoundOffset) % compoundRate === 0) {
+//             currentBalance += store;
+//             store = 0;
+//         }
+//         balanceArray.push(currentBalance);
+//     }
+
+//     return balanceArray;
+// };
+
+function createDataTable(accounts: Account[]): [Account, number[]][] {
+    const data: [Account, number[]][] = [];
+    accounts.forEach((account) => {
+        const row: number[] = new Array(13).fill(NaN);
+        row[0] = account.initialDeposit;
+        data.push([account, row]);
+    });
+    return data;
+}
+
+function enactDataTable(dataTable: [Account, number[]][]): [Account, number[]][] {
+    const data: [Account, number[]][] = dataTable.map(([account, dataRow]) => [account, dataRow]); // shallow copy, with buffer
+    const interestBuffer: number[] = new Array(data.length).fill(0);
+
+    // traverse by column
     for (let month = 1; month <= 12; month++) {
+        let balance = data[0][1][month - 1];
 
-        const interest = currentBalance * monthlyInterestRate;
-        store += interest;
+        // TODO: interest in savings last???
 
-        if ((month - compoundOffset) % compoundRate === 0) {
-            currentBalance += store;
-            store = 0;
+        for (let i = 0; i < data.length; i++) {
+            const [account, row] = data[i];
+
+            // calculate interest generated from previous month
+            if (row[month - 1] > 0) {
+                const monthlyInterestRate = account.annualInterestRate / 100 / 12;
+                const interest = row[month - 1] * monthlyInterestRate;
+                interestBuffer[i] += interest; //store interest in buffer
+            }
+
+            row[month] = row[month - 1];
+            if (month % account.compoundRate === 0) {
+                // only feed interest into balance if it's a compound month
+                row[month] += interestBuffer[i];
+                interestBuffer[i] = 0; // reset buffer
+            }
+
+            // alter balance
+            // currently, we just max out, where possible
+            const maxExpenditure = Math.min(account.maxInflow || 0, balance);
+            if (maxExpenditure > 0) {
+                balance -= maxExpenditure;
+                row[month] += maxExpenditure;
+                data[0][1][month] -= maxExpenditure;
+            }
         }
-        balanceArray.push(currentBalance);
     }
 
-    return balanceArray;
-};
+    return data;
+}
 
-const BankAccountTable: React.FC<BankAccountTableProps> = ({ accounts, mode, setTotalDelta}) => {
+const BankAccountTable: React.FC<BankAccountTableProps> = ({ accounts, mode, setTotalDelta }) => {
 
-    const visibleAccounts = accounts.filter(account =>
-        account.type.toLowerCase() === 'savings'
-        || (account.type.toLowerCase() === 'regular saver' && (mode === 'crs' && account.state?.toLowerCase() === 'owned') || mode === 'nrs')
-    );
+    const [dataTable, setDataTable] = React.useState<[Account, number[]][]>([]);
 
-    let totalDelta = 0;
+    // HANDLE STATE CHANGE
+    useEffect(() => {
+        // filter accounts
+        const visibleAccounts = accounts.filter(account =>
+            account.type.toLowerCase() === 'savings'
+            || (account.type.toLowerCase() === 'regular saver' && (mode === 'crs' && account.state?.toLowerCase() === 'owned') || mode === 'nrs')
+        );
 
-    const res = (
+        // generate data table
+        const tempDataTable = createDataTable(visibleAccounts);
+
+        // TODO: reverse-generate a yield table
+
+        // calculate data table
+        // tempDataTable.forEach(([account, row]) => {
+        //     const monthlyBalances = calculateMonthlyBalance(account.initialDeposit, account.annualInterestRate, account.compoundRate, account.compoundOffset);
+        //     // console.log(monthlyBalances);
+        //     for (let i = 1; i < row.length; i++) {
+        //         row[i] = monthlyBalances[i - 1];
+        //     }
+        // });
+        const newDataTable = enactDataTable(tempDataTable);
+
+        setDataTable(newDataTable);
+
+    }, [accounts, mode]);
+
+    useEffect(() => {
+        console.log(dataTable);
+        // handle total delta
+        const totalDelta = dataTable.reduce((sum, [, row]) => {
+            return sum + (row[row.length - 1] - row[0]);
+        }, 0);
+        setTotalDelta(totalDelta);
+
+    }, [dataTable]);
+
+    return (
         <div>
             <table>
                 <thead>
@@ -59,24 +142,23 @@ const BankAccountTable: React.FC<BankAccountTableProps> = ({ accounts, mode, set
                     </tr>
                 </thead>
                 <tbody>
-                    {visibleAccounts.map((account, index) => {
+                    {dataTable.map(([account, dataRow], index) => {
 
                         const ownedAccount = account.state?.toLowerCase() === 'owned';
-                        const monthlyBalances = calculateMonthlyBalance(account.initialDeposit, account.annualInterestRate, account.compoundRate, account.compoundOffset);
+                        // const monthlyBalances = calculateMonthlyBalance(account.initialDeposit, account.annualInterestRate, account.compoundRate, account.compoundOffset);
 
-                        const delta = monthlyBalances[monthlyBalances.length-1] - account.initialDeposit;
-                        totalDelta += delta;
+                        const delta = dataRow[dataRow.length - 1] - account.initialDeposit;
 
                         return (
                             <tr key={index}>
-                                <td><input type="checkbox" defaultChecked={ownedAccount} disabled={ownedAccount}/></td>
+                                <td><input type="checkbox" defaultChecked={ownedAccount} disabled={ownedAccount} /></td>
                                 <td>
-                                    <img src="https://www.shropshire-chamber.co.uk/wp-content/uploads/2021/06/Natwest-Logo.png" alt="Icon" width={26} height={32}/>
+                                    <img src="https://www.shropshire-chamber.co.uk/wp-content/uploads/2021/06/Natwest-Logo.png" alt="Icon" width={26} height={32} />
                                     {account.name}
                                 </td>
                                 <td>{account.annualInterestRate}%</td>
                                 <td className='column-border'>£{account.initialDeposit.toFixed(2)}</td>
-                                {monthlyBalances.map((balance, month) => (
+                                {dataRow.slice(1).map((balance, month) => (
                                     <td key={month}>£{balance.toFixed(2)}</td>
                                 ))}
                                 <td className='column-border'>£{delta.toFixed(2)}</td>
@@ -87,10 +169,6 @@ const BankAccountTable: React.FC<BankAccountTableProps> = ({ accounts, mode, set
             </table>
         </div>
     );
-
-    setTotalDelta(totalDelta);
-
-    return res;
 };
 
 export default BankAccountTable;
